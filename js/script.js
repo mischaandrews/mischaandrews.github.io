a1b96383a8408a814a2bf4e8fcaf61e209d7ca69
Mischa Andrews <mischaandrews@users.noreply.github.com>
2016-08-31 23:09:04 +1000

    No more 'open in new tab'

--- js/script.js
+++ js/script.js
@@ -8,18 +8,22 @@
 
 $(document).ready(function() {  
 
+	var openExternalLinksInNewTab = false;
+
+
+
 
 	/* EXTERNAL LINK CLASS */
 	
 	// Make all external links open in a new window, and mark them with the 'external' class
 	// ASSUMPTION: No internal links are relative (i.e. do not begin with 'http')
-	$("a").each(function(){
-		if ( $(this).attr('href') !== undefined && $(this).attr("href").substring(0, 4) === "http" ) {
-			$(this).attr("target", "_blank");
-			$(this).addClass("external");
-		}
-	});
-
-
-
+	if ( openExternalLinksInNewTab ) {
+		$("a").each(function(){
+			if ( $(this).attr('href') !== undefined && $(this).attr("href").substring(0, 4) === "http" ) {
+				$(this).attr("target", "_blank");
+				$(this).addClass("external");
+			}
+		});
+	}
+	
 });
