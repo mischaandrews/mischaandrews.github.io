ec42e4b19cb6ea6c906c2e2188734d1e29091c89
Mischa Andrews <mischaandrews@users.noreply.github.com>
2016-09-16 09:38:07 +1000

    Added online portfolio

---
+++ portfolio/js/script.js
@@ -0,0 +1,29 @@
+/*********************** MISC FUNCTIONS ***********************/
+
+
+
+
+
+/*********************** DOCUMENT READY ***********************/
+
+$(document).ready(function() {  
+
+	var openExternalLinksInNewTab = true;
+
+
+
+
+	/* EXTERNAL LINK CLASS */
+	
+	// Make all external links open in a new window, and mark them with the 'external' class
+	// ASSUMPTION: No internal links are relative (i.e. do not begin with 'http')
+	if ( openExternalLinksInNewTab ) {
+		$("a").each(function(){
+			if ( $(this).attr('href') !== undefined && $(this).attr("href").substring(0, 4) === "http" ) {
+				$(this).attr("target", "_blank");
+				$(this).addClass("external");
+			}
+		});
+	}
+	
+});
